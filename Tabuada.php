97ce9b83a05229beeb425e2257c7b1ea91c16e04
LuizOtavioMorais <luizmoraisap@protonmail.com>
2024-04-03 09:54:36 -0300

    Initial commit

---
+++ Tabuada.php
@@ -0,0 +1,15 @@
+<?php
+echo "Digite um NUMERO para obter a sua TABUADA:
+$numero = readline();
+echo "===================================" . PHP_EOL;
+echo $numero . " x 0 = " . $numero *0 . PHP_EOL;
+echo $numero . " x 1 = " . $numero *1 . PHP_EOL;
+echo $numero . " x 2 = " . $numero *2 . PHP_EOL;
+echo $numero . " x 3 = " . $numero *3 . PHP_EOL;
+echo $numero . " x 4 = " . $numero *4 . PHP_EOL;
+echo $numero . " x 5 = " . $numero *5 . PHP_EOL;
+echo $numero . " x 6 = " . $numero *6 . PHP_EOL;
+echo $numero . " x 7 = " . $numero *7 . PHP_EOL;
+echo $numero . " x 8 = " . $numero *8 . PHP_EOL;
+echo $numero . " x 9 = " . $numero *9 . PHP_EOL;
+echo "===================================";
