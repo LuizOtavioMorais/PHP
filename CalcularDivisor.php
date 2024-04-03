ba4dfe880358bc09f1fb9f558327e2d07c6f59ea
LuizOtavioMorais <luizmoraisap@protonmail.com>
2024-04-03 10:20:11 -0300

    calculardivisor

--- CalcularDivisor.php
+++ CalcularDivisor.php
@@ -1,1 +1,13 @@
 <?php
+echo "Escreva um numero para saber seus divisores: " . PHP_EOL;
+$n = readline();
+$divisor = 1;
+
+while($divisor < $n){
+    $resto = $n % $divisor;
+    if ($resto == 0){
+        echo "O numero " . $n . " é divisivel por " . $divisor . PHP_EOL;
+    }
+    ++$divisor;
+}
+
--- Tabuada.php
+++ Tabuada.php
@@ -1,7 +1,7 @@
 <?php
-echo "Digite um NUMERO para obter a sua TABUADA:
+echo "Digite um NUMERO para obter a sua TABUADA:";
 $numero = readline();
-echo "===================================" . PHP_EOL;
+echo "============" . PHP_EOL;
 echo $numero . " x 0 = " . $numero *0 . PHP_EOL;
 echo $numero . " x 1 = " . $numero *1 . PHP_EOL;
 echo $numero . " x 2 = " . $numero *2 . PHP_EOL;
@@ -12,4 +12,4 @@ echo $numero . " x 6 = " . $numero *6 . PHP_EOL;
 echo $numero . " x 7 = " . $numero *7 . PHP_EOL;
 echo $numero . " x 8 = " . $numero *8 . PHP_EOL;
 echo $numero . " x 9 = " . $numero *9 . PHP_EOL;
-echo "===================================";
+echo "============" ;
